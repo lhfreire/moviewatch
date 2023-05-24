0bcb00d9989130a90972d52ab63248a16a05ef0a
Luis Freire <luis.henrique.freire@gmail.com>
2023-05-24 14:58:20 -0300

    ajustes

---
+++ src/components/SideBar.tsx
@@ -0,0 +1,22 @@
+import '../styles/sidebar.scss';
+import { Icon } from '../components/Icon';
+
+console.log(Icon);
+
+const {  } = Icon;
+
+export function SideBar() {
+  return ( 
+    <div>
+      <div className="sidebar-genre">
+        <div>
+          
+        </div>
+        <div>
+
+        </div>
+      </div>
+      
+    </div>
+  );
+}
